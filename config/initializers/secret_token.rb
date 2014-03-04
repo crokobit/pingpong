291f31bcbb7d74cd92c8fe9835fff80c556dd4bc
Marcin Stecki <marcin@netguru.pl>
2014-03-04 11:29:30 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pingpong::Application.config.secret_key_base = 'a174f95da2d8136aa69c87490635b47d30b64b9f33291660acc76ba3e851be4a4a020081045bf773b39cf534437027595b1c57768a8068b1113be8e418d93fa7'
