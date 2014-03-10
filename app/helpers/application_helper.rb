27cfd9fd72ef9c62521a2078c385d8574c590dc3
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-10 20:59:04 +0100

    finish social buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,26 @@ module ApplicationHelper
     image_tag "//www.gravatar.com/avatar/#{hash}?s=#{size}", height: size, width: size
   end
 
-  def like_button link
-    content_tag(:div, class: 'fb-like-box pull-left') do
-      content_tag(:div, '', class: 'fb-like', data: { href: link, layout: 'button_count', show_faces: 'false', width: 250, send: 'false' })
+  def like_button url
+    content_tag(:div, class: 'fb-box pull-left') do
+      content_tag(:div, '', class: 'fb-like', data: { href: url, layout: 'button_count', show_faces: 'false', width: 250, send: 'false' })
+    end
+  end
+
+  def follow_on_fb_button url
+    content_tag(:div, class: 'fb-follow-box pull-left') do
+      content_tag(:div, '', class: 'fb-follow', data: { href: url, colorscheme: 'light', layout: 'button',show_faces: 'false' })
+    end
+  end
+
+  def follow_on_twitter_button url
+    content_tag(:div, class: 'twt-follow-box pull-left') do
+      link_to 'Follow @netguru', url, class: 'twitter-follow-button', data: { show_count: 'false', lans: 'en' }
     end
   end
 
   def tweet_button
-    content_tag(:div, class: 'tweet-box pull-left') do
+    content_tag(:div, class: 'twt-box pull-left') do
       link_to 'Tweet', 'ttps://twitter.com/share', class: 'twitter-share-button hidden-xs', data: { lang: 'en' }
     end
   end
