96547c2a7ff09ce1677ac7f786ea71e3b00542b6
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-10 20:40:04 +0100

    facebook like button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
     image_tag "//www.gravatar.com/avatar/#{hash}?s=#{size}", height: size, width: size
   end
 
+  def like_button link
+    content_tag(:div, class: 'fb-like-box pull-left') do
+      content_tag(:div, '', class: 'fb-like', data: { href: link, layout: 'button_count', show_faces: 'false', width: 250, send: 'false' })
+    end
+  end
+
+  def tweet_button
+    content_tag(:div, class: 'tweet-box pull-left') do
+      link_to 'Tweet', 'ttps://twitter.com/share', class: 'twitter-share-button hidden-xs', data: { lang: 'en' }
+    end
+  end
+
 end
