e335f1f945128f7b82fb9510507c3fb92b3182c8
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-12 12:39:52 +0100

    init pongs fight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
-  def gravatar_image_tag email = 'nobody@example.com', size = 150
+  def gravatar_image_tag email = 'nobody@example.com', size = 150, css_class = ''
     email ||= 'nobody@example.com'
     hash = Digest::MD5.hexdigest(email)
-    image_tag "//www.gravatar.com/avatar/#{hash}?s=#{size}", height: size, width: size
+    image_tag "//www.gravatar.com/avatar/#{hash}?s=#{size}", height: size, width: size, class: css_class
   end
 
   def like_button url
