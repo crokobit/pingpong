919e734718eec6fd5dc951dc59966fb5560df95e
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-03-14 16:28:57 +0100

    remove closing slash

--- app/models/pong.rb
+++ app/models/pong.rb
@@ -9,4 +9,12 @@ class Pong < ActiveRecord::Base
   scope :pending, -> { where(accepted: false).order(rating: :desc) }
   scope :from_user, -> (user){ where(user_id: user.id).order(created_at: :desc) }
 
+  after_save :remove_last_slash
+
+  def remove_last_slash
+    if self.gist.last == "/"
+      self.update_attributes(gist: self.gist[0..-2])
+    end
+  end
+
 end
