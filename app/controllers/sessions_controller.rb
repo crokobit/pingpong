36589f9de97622420b4647de64778267ec351f41
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-04-09 11:34:25 +0200

    remember correct location on vote

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,9 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
-    location = session[:location]
+    location = request.referer
     reset_session
     session[:user_id] = user.id
-
     if location.present?
       redirect_to location, :notice => 'Signed in!'
     else
